30e554bf9ce8408ff7e6ba635bf3d0796c38e35f
Jo Satoshi <jo@exglos.com>
2023-11-12 22:08:33 +0300

    state 1.1.0 no ethers add block

--- state/index_1_1_0.js
+++ state/index_1_1_0.js
@@ -1,9 +1,14 @@
 'use strict';
 
 (function () {
-    var ready, internalReady;
+    var loaded = {
+        txs: false,
+        internalTxs: false,
+        block: false
+    };
     var deposits, other, dividends, expenses; // hash, value, description
     var sales; // count, price, value
+    var block; // n, timestamp
 
     window.onload = function () {
         initButton('deposits');
@@ -13,9 +18,9 @@
         initButton('expenses');
 
         fillData();
-        print();
         readTxs();
         readInternalTxs();
+        readBlock();
     };
 
     function initButton(item) {
@@ -43,41 +48,34 @@
                 console.log(json.message);
             }
             json = json.result;
-            if (ready) {
-                return;
-            }
             for (var i = 0; i < json.length; i++) {
                 if (json[i].isError !== '0') {
                     console.error(json[i]);
                 } else if (json[i].to === '0x5b764b71d601bbd97434696c9f53ee2133f7e1cf') {
                     deposits.push({
                         hash: json[i].hash,
-                        value: ethers.utils.parseUnits(json[i].value, 'wei')
+                        value: new BigNumber(json[i].value)
                     });
                 } else if (json[i].functionName === 'profit()') {
-                    var value = ethers.utils.parseUnits(json[i].gasPrice, 'wei');
-                    value = value.mul(ethers.utils.parseUnits(json[i].gasUsed, 'wei'));
-                    value = value.add(ethers.utils.parseUnits(json[i].value, 'wei'));
                     dividends.push({
                         hash: json[i].hash,
-                        value: value
+                        value: new BigNumber(json[i].gasPrice).times(json[i].gasUsed).plus(json[i].value)
                     });
                 } else {
-                    var value = ethers.utils.parseUnits(json[i].gasPrice, 'wei');
-                    value = value.mul(ethers.utils.parseUnits(json[i].gasUsed, 'wei'));
-                    value = value.add(ethers.utils.parseUnits(json[i].value, 'wei'));
                     expenses.push({
                         hash: json[i].hash,
-                        value: value
+                        value: new BigNumber(json[i].gasPrice).times(json[i].gasUsed).plus(json[i].value)
                     });
                 }
             }
-            ready = true;
-            if (ready && internalReady) {
+            loaded.txs = true;
+            if (loaded.txs && loaded.internalTxs && loaded.block) {
                 print();
             }
         }).catch(function (error) {
             console.error(error);
+            fillData();
+            print();
         });
     }
 
@@ -96,28 +94,52 @@
                 console.log(json.message);
             }
             json = json.result;
-            if (internalReady) {
-                return;
-            }
             var currentSalesRound = sales[sales.length - 1];
             for (var i = 0; i < json.length; i++) {
                 if (json[i].isError !== '0') {
                     console.error(json[i]);
                 } else if (json[i].from === '0x3ddee7cdf8d71490b518b1e6e6f2198433636903') {
                     currentSalesRound.count++;
-                    currentSalesRound.value =
-                        currentSalesRound.value.add(ethers.utils.parseUnits(json[i].value, 'wei'));
+                    currentSalesRound.value = currentSalesRound.value.plus(json[i].value);
                 } else if (json[i].to === '0x5b764b71d601bbd97434696c9f53ee2133f7e1cf') {
                     other.push({
                         hash: json[i].hash,
-                        value: ethers.utils.parseUnits(json[i].value, 'wei')
+                        value: new BigNumber(json[i].value)
                     });
                 } else {
                     console.error(json[i]);
                 }
             }
-            internalReady = true;
-            if (ready && internalReady) {
+            loaded.internalTxs = true;
+            if (loaded.txs && loaded.internalTxs && loaded.block) {
+                print();
+            }
+        }).catch(function (error) {
+            console.error(error);
+            fillData();
+            print();
+        });
+    }
+
+    function readBlock() {
+        block.timestamp = (new Date().getTime() / 1000).toFixed();
+        var url = 'https://api.etherscan.io/api?module=block&action=getblocknobytime' +
+            '&timestamp=' + block.timestamp +
+            '&closest=before&apikey=6KYMVV3GRFU2MSXKJKXEMHUNFD52ME3EIV';
+        fetch(url).then(function (response) {
+            if (!response.ok) {
+                response.text().then(console.error);
+                throw new Error(response.status);
+            }
+            return response.json();
+        }).then(function (json) {
+            if (json.status !== '1' || isNaN(json.result)) {
+                console.log(json.message);
+            } else {
+                block.n = json.result;
+            }
+            loaded.block = true;
+            if (loaded.txs && loaded.internalTxs && loaded.block) {
                 print();
             }
         }).catch(function (error) {
@@ -126,11 +148,11 @@
     }
 
     function print() {
-        var sum = ethers.constants.Zero;
+        var sum = new BigNumber(0);
         var container = document.getElementById('depositsList');
         container.innerHTML = '';
         for (var i = deposits.length - 1; i >= 0; i--) {
-            sum = sum.add(deposits[i].value);
+            sum = sum.plus(deposits[i].value);
             var div = document.createElement('div');
             var p = document.createElement('p');
             p.innerHTML = formatWei(deposits[i].value);
@@ -144,11 +166,11 @@
         document.getElementById('deposits').innerHTML = formatWei(sum);
         var inputs = sum;
 
-        sum = ethers.constants.Zero;
+        sum = new BigNumber(0);
         container = document.getElementById('salesList');
         container.innerHTML = '';
         for (var i = sales.length - 1; i >= 0; i--) {
-            sum = sum.add(sales[i].value);
+            sum = sum.plus(sales[i].value);
             var div = document.createElement('div');
             var p = document.createElement('p');
             p.innerHTML = formatWei(sales[i].value);
@@ -159,13 +181,13 @@
             container.append(div);
         }
         document.getElementById('sales').innerHTML = formatWei(sum);
-        inputs = inputs.add(sum);
+        inputs = inputs.plus(sum);
 
-        sum = ethers.constants.Zero;
+        sum = new BigNumber(0);
         container = document.getElementById('otherList');
         container.innerHTML = '';
         for (var i = other.length - 1; i >= 0; i--) {
-            sum = sum.add(other[i].value);
+            sum = sum.plus(other[i].value);
             var div = document.createElement('div');
             var p = document.createElement('p');
             p.innerHTML = formatWei(other[i].value);
@@ -177,14 +199,14 @@
             container.append(div);
         }
         document.getElementById('other').innerHTML = formatWei(sum);
-        inputs = inputs.add(sum);
+        inputs = inputs.plus(sum);
         document.getElementById('inputs').innerHTML = formatWei(inputs);
 
-        sum = ethers.constants.Zero;
+        sum = new BigNumber(0);
         var container = document.getElementById('dividendsList');
         container.innerHTML = '';
         for (var i = dividends.length - 1; i >= 0; i--) {
-            sum = sum.add(dividends[i].value);
+            sum = sum.plus(dividends[i].value);
             var div = document.createElement('div');
             var p = document.createElement('p');
             p.innerHTML = formatWei(dividends[i].value, true);
@@ -198,11 +220,11 @@
         document.getElementById('dividends').innerHTML = formatWei(sum, true);
         var outputs = sum;
 
-        sum = ethers.constants.Zero;
+        sum = new BigNumber(0);
         var container = document.getElementById('expensesList');
         container.innerHTML = '';
         for (var i = expenses.length - 1; i >= 0; i--) {
-            sum = sum.add(expenses[i].value);
+            sum = sum.plus(expenses[i].value);
             var div = document.createElement('div');
             var p = document.createElement('p');
             p.innerHTML = formatWei(expenses[i].value, true);
@@ -214,19 +236,23 @@
             container.append(div);
         }
         document.getElementById('expenses').innerHTML = formatWei(sum, true);
-        outputs = outputs.add(sum);
+        outputs = outputs.plus(sum);
         document.getElementById('outputs').innerHTML = formatWei(outputs, true);
 
-        document.getElementById('balance').innerHTML = formatWei(inputs.sub(outputs));
+        document.getElementById('balance').innerHTML = formatWei(inputs.minus(outputs));
+
+        var date = new Date(block.timestamp * 1000 - new Date().getTimezoneOffset() * 60000);
+        document.getElementById('time').innerHTML = 'block ' + block.n +
+            ' (' + date.toISOString().substr(0, 19) + ')';
     }
 
     function formatWei(value, negate) {
-        value = ethers.utils.formatEther(value).split('.');
-        while (value[1].length < 18) {
-            value[1] += '0';
+        if (negate) {
+            value = value.negated();
         }
-        return (negate ? '-' : '') + value[0] + '.' + value[1].substring(0, 3) +
-            '<span class="small">' + value[1].substring(3) + '</span>';
+        value = value.shiftedBy(-18).toFixed(18);
+        return value.substring(0, value.length - 15) +
+            '<span class="small">' + value.substring(value.length - 15) + '</span>';
     }
 
     function formatHash(value) {
@@ -237,119 +263,122 @@
     function fillData() {
         deposits = [{
             hash: '0xc58645950319ad5d5543cb930e1aaab2de9f8bdafedc947cbc16d30e634b1db8',
-            value: ethers.utils.parseEther('0.479444')
+            value: new BigNumber('0.479444').shiftedBy(18)
         }, {
             hash: '0x26a0d370ccc05a22a13b85d9efeb976616055ae9e25693e4f7f79cef52147e0b',
-            value: ethers.utils.parseEther('0.049279497772226')
+            value: new BigNumber('0.049279497772226').shiftedBy(18)
         }];
         sales = [{
             count: 20,
             price: 0.002,
-            value: ethers.utils.parseEther('2.146292891017674449')
+            value: new BigNumber('2.146292891017674449').shiftedBy(18)
         }, {
             count: 3,
             price: 0.004,
-            value: ethers.utils.parseEther('0.257100174004222008')
+            value: new BigNumber('0.257100174004222008').shiftedBy(18)
         }];
         other = [];
         dividends = [{
             hash: '0x699a85c4b0f017e64446b0ea6e2c09f89e89e3e188dac0e341cfa60d83319574',
-            value: ethers.utils.parseEther('0.010790229395029134'),
+            value: new BigNumber('0.010790229395029134').shiftedBy(18),
             description: '0.000199 eth/exg'
         }, {
             hash: '0x03db0da10306b490b769c663e773f5428596103b0e02c02fb27cf844a703a157',
-            value: ethers.utils.parseEther('0.100178428'),
+            value: new BigNumber('0.100178428').shiftedBy(18),
             description: '0.0004 eth/exg'
         }, {
             hash: '0xa6a91c836928c824612523743238bb6278bd3e5fd66aabd54e74a437b3fd6219',
-            value: ethers.utils.parseEther('0.100147051755259002'),
+            value: new BigNumber('0.100147051755259002').shiftedBy(18),
             description: '0.0004 eth/exg'
         }, {
             hash: '0xe378d80c623a2cf8a9a18a1487dcb695c6dc7fe651d1525b66b12a7e7959cbaf',
-            value: ethers.utils.parseEther('0.070267642'),
+            value: new BigNumber('0.070267642').shiftedBy(18),
             description: '0.000278 eth/exg'
         }, {
             hash: '0x46d42e8e343c0526c44960b064e287bd7af3c32325aae77205a070184d80b3f4',
-            value: ethers.utils.parseEther('0.160475808'),
+            value: new BigNumber('0.160475808').shiftedBy(18),
             description: '0.000538 eth/exg'
         }, {
             hash: '0x8785bfe2fece1338c52d2de8e1a770422a5ee962dccaac27e1cdec7e4ba0d462',
-            value: ethers.utils.parseEther('0.700921878'),
+            value: new BigNumber('0.700921878').shiftedBy(18),
             description: '0.000936 eth/exg'
         }, {
             hash: '0x2f67cfd714f2523c842fb66689984cb56e27f121e0f03a387d6286ebced16906',
-            value: ethers.utils.parseEther('0.300535284'),
+            value: new BigNumber('0.300535284').shiftedBy(18),
             description: '0.000283 eth/exg'
         }];
         expenses = [{
             hash: '0x1ba58b69b3b567205be79e579e934463e1104a9f754284c129e18f3d6a33692c',
-            value: ethers.utils.parseEther('0.02021215530733306'),
+            value: new BigNumber('0.02021215530733306').shiftedBy(18),
             description: '<a href="https://etherscan.io/address/0x3ddee7cdf8d71490b518b1e6e6f2198433636903" target="_blank">exglos contract</a> creation'
         }, {
             hash: '0x886e96ae72a6203700fa1b483468ce5c7fdb469dd52f1ffad1e2da7bb2ab8acf',
-            value: ethers.utils.parseEther('0.020517421187449822'),
+            value: new BigNumber('0.020517421187449822').shiftedBy(18),
             description: 'domain <a href="https://exglos.com" target="_blank">exglos.com</a>'
         }, {
             hash: '0x4307a81bf8534d6ba6220213e1946e2869be029f9cc66983fec6fbca6f851acd',
-            value: ethers.utils.parseEther('0.000351032'),
+            value: new BigNumber('0.000351032').shiftedBy(18),
             description: 'set canonical url'
         }, {
             hash: '0xf686497e5a8f7af359ec5c880153fddf51bcfd78e2ab499a70136420eeafa918',
-            value: ethers.utils.parseEther('0.071210822691926'),
+            value: new BigNumber('0.071210822691926').shiftedBy(18),
             description: '<a href="https://hyip-check.ru/project/5649" target="_blank">monitoring</a>'
         }, {
             hash: '0xef6708df4fa43f37319151f6a5e9791fe2f68e3b9728aaa3e459fd2b573790ed',
-            value: ethers.utils.parseEther('0.070233455861884'),
+            value: new BigNumber('0.070233455861884').shiftedBy(18),
             description: 'Jo salary 2022'
         }, {
             hash: '0x1ca90df70d9f3072727c3071c61ea309d18cc7ea919bbd7ebd1f5a355e6f8d36',
-            value: ethers.utils.parseEther('0.080110226322878'),
+            value: new BigNumber('0.080110226322878').shiftedBy(18),
             description: '<a href="https://www.mabnews.com/exglos.com-review.html" target="_blank">blog</a>'
         }, {
             hash: '0x8f6089c37e0df6d6ae72eafa62b011a8203f40886044acf375eeb3907160f47c',
-            value: ethers.utils.parseEther('0.100105'),
+            value: new BigNumber('0.100105').shiftedBy(18),
             description: '<a href="https://onlinebitz.net/exglos.com-review" target="_blank">another blog</a>'
         }, {
             hash: '0x7ddbf89bfaa7dfc1055d33a5caf8f7067baf94e1254682a43f226607d50f1c4f',
-            value: ethers.utils.parseEther('0.010259180000000002'),
+            value: new BigNumber('0.010259180000000002').shiftedBy(18),
             description: '10% platform fee for audit'
         }, {
             hash: '0x01580ffb51087459a4ebfbe68d74c612fd0ccfc9bb461ea53a80a143cfd8b8ef',
-            value: ethers.utils.parseEther('0.100189'),
+            value: new BigNumber('0.100189').shiftedBy(18),
             description: '<a href="https://github.com/exglos/contract/blob/main/contracts/audits/byNaftaliMurgor.md" target="_blank">smart contract audit</a>'
         }, {
             hash: '0x0817aa9dcf255d81d332211af0e50e548781677429e07958fa73455f5b95df29',
-            value: ethers.utils.parseEther('0.12542'),
+            value: new BigNumber('0.12542').shiftedBy(18),
             description: 'building <a href="https://start.exglos.com" target="_blank">start.exglos.com</a>'
         }, {
             hash: '0x3580da3d925ad736bd99a9048c9cdf320dfbc4ba05b66fb406a3df0989440a13',
-            value: ethers.utils.parseEther('0.000693825'),
+            value: new BigNumber('0.000693825').shiftedBy(18),
             description: 'ens unsuccessful commit'
         }, {
             hash: '0xe465f83882fc572b340258f502e04cb56d3fefbb402ffa0df2677835b0546019',
-            value: ethers.utils.parseEther('0.000740272'),
+            value: new BigNumber('0.000740272').shiftedBy(18),
             description: 'ens successful commit'
         }, {
             hash: '0xd28f341850fcf83452842605dfadf58a99c15f6e75a58f8475c6a21a2d272e3a',
-            value: ethers.utils.parseEther('0.033055870791071075').sub(
-                ethers.utils.parseEther('0.002631284617370097')),
+            value: new BigNumber('0.033055870791071075').minus('0.002631284617370097').shiftedBy(18),
             description: 'registration <a href="https://etherscan.io/enslookup-search?search=exglos.eth" target="_blank">exglos.eth</a>'
         }, {
             hash: '0x5568932e0e7bc5fceb67689ca1f2dcf96dff26bf7e18902385642160a97a9db1',
-            value: ethers.utils.parseEther('0.002485512297907646'),
+            value: new BigNumber('0.002485512297907646').shiftedBy(18),
             description: 'reverse ens'
         }, {
             hash: '0xc03726ce97c86b8ee70b6ab436d7816ed0735d066daadc594ccb02565f913484',
-            value: ethers.utils.parseEther('0.00059528'),
+            value: new BigNumber('0.00059528').shiftedBy(18),
             description: 'set price 0.004'
         }, {
             hash: '0x4b4948f941749cca90fdbaa5af8ffefe6053378a3c781e074d5b8109261832cb',
-            value: ethers.utils.parseEther('0.006375690215145'),
+            value: new BigNumber('0.006375690215145').shiftedBy(18),
             description: 'to contest winner'
         }, {
             hash: '0x26603cf756a19638dcc0bf4616eabf5151e68f5ae515bacb7ce1c11c36121f5a',
-            value: ethers.utils.parseEther('0.070315'),
+            value: new BigNumber('0.070315').shiftedBy(18),
             description: '50% of development <a href="https://ens.exglos.com" target="_blank">ens.exglos.com</a>'
         }];
+        block = {
+            n: 18536000,
+            timestamp: 1699552343
+        };
     }
 })();
